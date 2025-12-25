305b7de50a1d7648ed45d193cb9d9f89cad02d6d
roundRekt <roundrekt@icloud.com>
2025-12-25 18:45:13 +0800

    响应服务器信息或反向代理

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+  async fetch(request, env) {
+	const key = new URL(request.url).pathname.slice(1);
+    const value = await env.SERVERINFO.get(key);
+
+    if (value) {
+      return new Response(value, {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    return fetch(
+      "https://yostar-serverinfo.bluearchiveyostar.com/" + key
+    );
+  },
 };
